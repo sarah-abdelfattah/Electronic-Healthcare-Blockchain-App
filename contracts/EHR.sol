516106dffdb6d5ee19e399c9c762cf702630e431
sarah23s <sarah_23s@hotmail.com>
2022-06-07 23:40:54 +0200

    Initial contract architecture

--- contracts/EHR.sol
+++ contracts/EHR.sol
@@ -1,8 +1,96 @@
 pragma solidity ^0.5.0;
 
 contract EHR {
-  uint public patients = 0;
-  uint public clinics = 0;
-  uint public regularVisits = 0;
-  uint public labVisits = 0;
-} 
+    enum VisitTypes {
+        periodicCheckup,
+        caseManagement,
+        complain
+    }
+
+    int256 public patientsCount = 0;
+    int256 public clinicsCount = 0;
+    int256 public regularVisitsCount = 0;
+    int256 public labVisitsCount = 0;
+
+    struct Clinic {
+        int256 clinicID;
+        string location;
+        int256 numberOfPatients;
+        // Patient[] patients;
+        mapping(uint256 => Patient) patients;
+    }
+
+    struct Patient {
+        int256 patientID;
+        string name;
+        int256 age;
+        int256 weight;
+        int256 height;
+        string gender;
+        string initialBloodPressure;
+        string initialHeartRate;
+        string initialTemperature;
+        int256 regularVisitCounts;
+        int256 labVisitsCount;
+        int256 numberOfRegularVisits;
+        int256 numberOfLabVisits;
+        mapping(uint256 => RegularVisit) regularVisits;
+        mapping(uint256 => LabVisit) labVisits;
+    }
+
+    struct RegularVisit {
+        int256 regularVisitID;
+        int256 visitBloodPressure;
+        int256 visitHeartRate;
+        int256 visitTemperature;
+        string diagnosis;
+        VisitTypes visitType;
+        Prescription prescription;
+    }
+
+    struct LabVisit {
+        int256 labVisitID;
+        int256 visitBloodPressure;
+        int256 visitHeartRate;
+        int256 visitTemperature;
+        string testType;
+        string testResult;
+    }
+
+    struct Prescription {
+        string referral;
+        string followUp;
+        string lab;
+        int256 numberOfMedicines;
+        mapping(uint256 => Medicine) medicines;
+    }
+
+    struct Medicine {
+        string name;
+        string dose;
+        string period;
+    }
+
+    mapping(int256 => Clinic) public clinics;
+
+    constructor() public {
+        createClinic("Egypt");
+    }
+
+    // mapping(uint => Patient) public patients;
+    // mapping(uint => RegularVisit) public regularVisits;
+    // mapping(uint => LabVisit) public labVisits;
+
+    function createClinic(string memory _location) public {
+        clinicsCount++;
+        // Patient[] memory emptyPatients;
+        Clinic memory _clinic = Clinic(
+            clinicsCount,
+            _location,
+            0
+            // emptyPatients
+        );
+
+        clinics[clinicsCount] = _clinic;
+    }
+}
